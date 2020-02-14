ba24f32f3f90a3274c38cb937b3aab55a36fe188
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-14 16:04:46 -0500

    Initial commit of spring boot project

---
+++ src/main/java/com/environmentsetup/EnvironmentsetupApplication.java
@@ -0,0 +1,13 @@
+package com.environmentsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentsetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvironmentsetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/environmentsetup/environmentsetup/EnvironmentsetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.environmentsetup.environmentsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentsetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
